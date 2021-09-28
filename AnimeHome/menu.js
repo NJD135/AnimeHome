bd7a673814a8d327c7db7f4879cf970c953318fc
Nabajyoti Das <80690055+NJD135@users.noreply.github.com>
2021-09-28 20:26:38 +0530

    Add files via upload

---
+++ AnimeHome/menu.js
@@ -0,0 +1,6 @@
+function openSideMenu() {
+    document.getElementById('sideMenu').style.width = '250px';
+}
+function closeSideMenu() {
+    document.getElementById('sideMenu').style.width = '0';
+}
